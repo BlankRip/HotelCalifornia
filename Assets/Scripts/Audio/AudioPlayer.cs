0d5634ca6b629003ba6dfbba551799b1e33e1577
BlankRip <aniketh_g5@outlook.com>
2021-09-21 21:13:11 +0400

    Object pool added, 3d audio use pool now

--- Assets/Scripts/Audio/AudioPlayer.cs
+++ Assets/Scripts/Audio/AudioPlayer.cs
@@ -1,23 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Knotgames.Gameplay;
 
 namespace Knotgames.Audio
 {
     public class AudioPlayer : MonoBehaviour
     {
-        public static AudioPlayer audioPlayer;
+        public static AudioPlayer instance;
 
         [SerializeField] ScriptableAudioDatabase audioDatabase;
         [SerializeField] AudioSource sfxSource2D;
-        [SerializeField] AudioSource sfxSource3D;
+        private AudioSource sfxSource3D;
 
         private void Awake()
         {
+            if(instance == null)
+                instance = this;
+            else
+                Destroy(this);
+
             sfxSource2D.loop = false;
-            sfxSource3D.loop = false;
             sfxSource2D.playOnAwake = false;
-            sfxSource3D.playOnAwake = false;
         }
 
         public void PlayAudio2D(ClipName name)
@@ -32,8 +36,9 @@ namespace Knotgames.Audio
             sfxSource2D.PlayOneShot(audioDatabase.GetClip(name));
         }
 
-        public void PlayAudio3D(ClipName name)
+        public void PlayAudio3D(ClipName name, Vector3 position)
         {
+            sfxSource3D = ObjectPool.instance.SpawnPoolObj("3dSource", position, Quaternion.identity).GetComponent<AudioSource>();
             sfxSource3D.Stop();
             sfxSource3D.clip = audioDatabase.GetClip(name);
             sfxSource3D.Play();
@@ -41,7 +46,8 @@ namespace Knotgames.Audio
 
         public void PlayAudio3DOneShot(ClipName name)
         {
-            sfxSource3D.PlayOneShot(audioDatabase.GetClip(name));
+            if(sfxSource3D != null)
+                sfxSource3D.PlayOneShot(audioDatabase.GetClip(name));
         }
     }
 }
