f9dc2d5dd224b3f1dc412832f669902caa56e51a
BlankRip <aniketh_g5@outlook.com>
2021-09-26 18:57:40 +0400

    You come as human I come as ghost

--- Assets/Scripts/Gameplay/HumanAbilityEffectTrigger.cs
+++ Assets/Scripts/Gameplay/HumanAbilityEffectTrigger.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Knotgames.Network;
 
 namespace Knotgames.Gameplay {
     public class HumanAbilityEffectTrigger : MonoBehaviour, IAbilityEffectTrigger
     {
         //^IAbilityEffect testEffect;
+        [SerializeField] NetObject netObj;
+        private NetSendData dataToSend;
+
         private bool underEffect;
         private float timer;
         private bool onTimer;
@@ -15,7 +19,29 @@ namespace Knotgames.Gameplay {
 
 
         private void Start() {
+            if(netObj == null)
+                netObj = GetComponent<NetObject>();
+            dataToSend = new NetSendData(netObj.id, 0, 0);
+            
             blurEffect = GetComponent<BlurEffect>();
+            netObj.OnMessageRecieve += RecieveData;
+        }
+
+        private void OnDestroy() {
+            netObj.OnMessageRecieve -= RecieveData;
+        }
+
+        private void SendData(int effectType, float duration) {
+            dataToSend.effectType = effectType;
+            dataToSend.duration = duration;
+            NetConnector.instance.SendDataToServer(JsonUtility.ToJson(dataToSend));
+        }
+
+        private void RecieveData(string recieved) {
+            if(JsonUtility.FromJson<ObjectNetData>(recieved).componentType == "EffectTrigger") {
+                DataExtraction extracted = JsonUtility.FromJson<DataExtraction>(recieved);
+                TriggerEffect((AbilityEffectType)extracted.effectType, extracted.duration, false);
+            }
         }
 
         private void Update() {
@@ -30,7 +56,7 @@ namespace Knotgames.Gameplay {
             }
         }
 
-        public void TriggerEffect(AbilityEffectType type, float duration) {
+        public void TriggerEffect(AbilityEffectType type, float duration, bool sendData) {
             switch(type) {
                 case AbilityEffectType.BlurEffect:
                     blurEffect.ApplyEffect();
@@ -41,10 +67,44 @@ namespace Knotgames.Gameplay {
             underEffect = true;
             timer = duration;
             onTimer = true;
+
+            if(sendData)
+                SendData((int) type, duration);
         }
 
         public bool IsUnderEffect() {
             return underEffect;
+        }
+
+        private class DataExtraction 
+        {
+            public int effectType;
+            public float duration;
+        }
+
+        [System.Serializable]
+        private class NetSendData
+        {
+            public string eventName;
+            public string componentType;
+            public string distributionOption;
+            //public string ownerID;
+            public string objectID;
+            public string roomID;
+            public int effectType;
+            public float duration;
+
+            public NetSendData(string netId, int effectType, float duration) {
+                eventName = "syncObjectData";
+                distributionOption = DistributionOption.serveOthers;
+                componentType = "EffectTrigger";
+                if(!DevBoy.yes)
+                {
+                    //ownerID = NetConnector.instance.playerID.value;
+                    roomID = NetRoomJoin.instance.roomID.value;
+                }
+                objectID = netId;
+            }
         }
     }
 }
