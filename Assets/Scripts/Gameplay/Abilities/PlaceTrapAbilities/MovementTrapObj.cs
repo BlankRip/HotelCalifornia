b0aae2736ab3337d08c1f29d1528d0246bd4a66e
BlankRip <aniketh_g5@outlook.com>
2021-09-30 13:19:23 +0400

    Spawn destroy not networked

--- Assets/Scripts/Gameplay/Abilities/PlaceTrapAbilities/MovementTrapObj.cs
+++ Assets/Scripts/Gameplay/Abilities/PlaceTrapAbilities/MovementTrapObj.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Knotgames.Network;
 
 namespace Knotgames.Gameplay.Abilities {
     public class MovementTrapObj : MonoBehaviour, IMovementTrap
     {
+        [SerializeField] NetObject netObj;
         [SerializeField] Transform trapProjection;
         [SerializeField] Transform nutrelizerProjection;
         [SerializeField] Transform trap;
@@ -13,9 +15,21 @@ namespace Knotgames.Gameplay.Abilities {
         private float maxDistance = 20;
 
         private void Start() {
-            trapProjection.gameObject.SetActive(true);
-            nutrelizerProjection.gameObject.SetActive(true);
-            maxDistance = maxDistance * maxDistance;
+            if(netObj == null)
+                netObj = GetComponent<NetObject>();
+
+            if(DevBoy.yes || netObj.IsMine) {
+                trapProjection.gameObject.SetActive(true);
+                nutrelizerProjection.gameObject.SetActive(true);
+                maxDistance = maxDistance * maxDistance;
+            }
+        }
+
+        private void RecieveData(string recieved) {
+            if(JsonUtility.FromJson<ObjectNetData>(recieved).componentType == "MoveTrap") {
+                DataExtraction extracted = JsonUtility.FromJson<DataExtraction>(recieved);
+                SetTrap(extracted.trapPos, extracted.nutralizerPos, extracted.nutralizerRot.ToQuaternion());
+            }
         }
 
         public void MoveTrapTo(Vector3 position) {
@@ -31,9 +45,14 @@ namespace Knotgames.Gameplay.Abilities {
         }
 
         public void SetTrap() {
-            trap.position = trapProjection.position;
-            nutrelizer.position = nutrelizerProjection.position;
-            nutrelizer.rotation = nutrelizerProjection.rotation;
+            SetTrap(trapProjection.position, nutrelizerProjection.position, nutrelizerProjection.rotation);
+            SendTrapData();
+        }
+
+        private void SetTrap(Vector3 trapPos, Vector3 nutralizerPos, Quaternion nutralizerRot) {
+            trap.position = trapPos;
+            nutrelizer.position = nutralizerPos;
+            nutrelizer.rotation = nutralizerRot;
 
             trapProjection.gameObject.SetActive(false);
             nutrelizerProjection.gameObject.SetActive(false);
@@ -41,8 +60,46 @@ namespace Knotgames.Gameplay.Abilities {
             nutrelizer.gameObject.SetActive(true);
         }
 
+        private void SendTrapData() {
+            NetConnector.instance.SendDataToServer(JsonUtility.ToJson(new NetSendData(netObj.id, trapProjection.position, 
+                nutrelizerProjection.position, new RotationWS(nutrelizerProjection.rotation))));
+        }
+
         public void DestroyTrap() {
             Destroy(this.gameObject);
+        }
+
+        private class DataExtraction 
+        {
+            public Vector3 trapPos;
+            public Vector3 nutralizerPos;
+            public RotationWS nutralizerRot;
+        }
+
+        [System.Serializable]
+        private class NetSendData
+        {
+            public string eventName;
+            public string componentType;
+            public string distributionOption;
+            public string objectID;
+            public string roomID;
+            public Vector3 trapPos;
+            public Vector3 nutralizerPos;
+            public RotationWS nutralizerRot;
+
+            public NetSendData(string netId, Vector3 trapPos, Vector3 nutralizerPos, RotationWS nutralizerRot) {
+                eventName = "syncObjectData";
+                distributionOption = DistributionOption.serveOthers;
+                componentType = "MoveTrap";
+                if(!DevBoy.yes) {
+                    roomID = NetRoomJoin.instance.roomID.value;
+                }
+                objectID = netId;
+                this.trapPos = trapPos;
+                this.nutralizerPos = nutralizerPos;
+                this.nutralizerRot = nutralizerRot;
+            }
         }
     }
 }
