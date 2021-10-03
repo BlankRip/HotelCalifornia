80a47b1d82c1a62af98f13f81599af0b59ebc51f
BlankRip <aniketh_g5@outlook.com>
2021-10-03 22:41:41 +0400

    Using Pooler to get the text object

--- Assets/Scripts/Gameplay/Puzzles/SolutionPad.cs
+++ Assets/Scripts/Gameplay/Puzzles/SolutionPad.cs
@@ -2,23 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Knotgames.Gameplay.UI;
 
 namespace Knotgames.Gameplay.Puzzle {
     public class SolutionPad : MonoBehaviour
     {
-        [SerializeField] TextMeshProUGUI myText;
         [SerializeField] Transform targetPos;
+        private TextMeshProUGUI myText;
 
-        private void Update() {
-            myText.transform.position = targetPos.position;
-            myText.transform.rotation = targetPos.rotation;
-        }
+        // private void Update() {
+        //     myText.transform.position = targetPos.position;
+        //     myText.transform.rotation = targetPos.rotation;
+        // }
 
         public void UpdateText(string textValue) {
+            myText = ObjectPool.instance.SpawnPoolObj("ChemPadText", targetPos.position, targetPos.rotation).GetComponent<TextMeshProUGUI>();
             myText.text = textValue;
-
-            // myText.transform.position = targetPos.position;
-            // myText.transform.rotation = targetPos.rotation;
 
             this.gameObject.SetActive(true);
             myText.gameObject.SetActive(true);
