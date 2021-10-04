45f78633af2d3b27ce347db7f1d3a88633405c70
BlankRip <aniketh_g5@outlook.com>
2021-10-04 08:35:41 +0400

    Crouch code no animation

--- Assets/Scripts/Gameplay/HumanMovement.cs
+++ Assets/Scripts/Gameplay/HumanMovement.cs
@@ -10,13 +10,20 @@ namespace Knotgames.Gameplay {
         [SerializeField] float gravity = -19.2f;
         [SerializeField] float jumpHight = 5;
         [SerializeField] [Range(0, 1)] float crouchSpeedMultiplier = 0.5f;
+        [SerializeField] [Range(0, 1)] float crouchHightReductionMultipiler = 0.7f;
         private Vector3 velocity;
+        private float initialHight;
+        private float crouchHight;
         private bool grounded;
         private bool inJump;
 
         private void Start() {
             if(cc == null)
                 cc = GetComponent<CharacterController>();
+            
+            initialHight = cc.height;
+            crouchHight = initialHight * (1 - crouchHightReductionMultipiler);
+            Debug.Log(crouchHight);
             
             if(gravity > 0)
                 gravity *= -1;
@@ -27,6 +34,12 @@ namespace Knotgames.Gameplay {
 
         public void Move(float horizontalInput, float verticalInput, ref bool moveYPositive, ref bool moveYNegetive)
         {
+            if(CrouchUnderEffect(moveYNegetive)) {
+                velocity.y += gravity * Time.deltaTime;
+                cc.Move(velocity * Time.deltaTime);
+                return;
+            }
+
             float speed = movementSpeed;
             if(moveYNegetive)
                 speed *= crouchSpeedMultiplier;
@@ -47,9 +60,26 @@ namespace Knotgames.Gameplay {
                 } else
                     moveYPositive = false;
             }
-
             velocity.y += gravity * Time.deltaTime;
             cc.Move(velocity * Time.deltaTime);
+        }
+
+        private bool CrouchUnderEffect(bool crouching) {
+            if(crouching && grounded) {
+                float difference = Mathf.Abs(cc.height - crouchHight);
+                if(difference > 0.005f) {
+                    cc.height = Mathf.Lerp(cc.height, crouchHight, Time.deltaTime * 10);
+                }
+                return true;
+            } else {
+                float difference = Mathf.Abs(cc.height - initialHight);
+                if(difference > 0.005f) {
+                    cc.height = Mathf.Lerp(cc.height, initialHight, Time.deltaTime * 10);
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         public void MoveToPosition(Vector3 position) {
