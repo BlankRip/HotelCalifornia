8287826cb51e12cf126137115ba738591a3067fd
BlankRip <aniketh_g5@outlook.com>
2021-10-17 14:04:19 +0400

    Ghost interfere works, the puzzle is good offline needs netsync

--- Assets/Scripts/Gameplay/Puzzles/RiddlerRoom/RiddleBoard.cs
+++ Assets/Scripts/Gameplay/Puzzles/RiddlerRoom/RiddleBoard.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Knotgames.Gameplay.Abilities;
 
 namespace Knotgames.Gameplay.Puzzle.Riddler {
-    public class RiddleBoard : MonoBehaviour
+    public class RiddleBoard : MonoBehaviour, IInterfear
     {
         [SerializeField] string textPoolTag;
         [SerializeField] Transform textPos;
@@ -12,7 +13,12 @@ namespace Knotgames.Gameplay.Puzzle.Riddler {
         [SerializeField] RiddleSolutionPad mySolutionPad;
         private TextMeshProUGUI myText;
         private Riddle myRiddle;
+
         private RiddlerInputPanel inputPanel;
+        private bool underSpell;
+        private bool timerOn;
+        private float timer;
+        private float spellDuration = 15;
 
         private void Start() {
             inputPanel = FindObjectOfType<RiddlerInputPanel>();
@@ -26,8 +32,34 @@ namespace Knotgames.Gameplay.Puzzle.Riddler {
             mySolutionPad.SetSolution(myRiddle.answer, inputPanel);
         }
 
+        private void Update() {
+            if(timerOn) {
+                timer += Time.deltaTime;
+                if(timer >= spellDuration) {
+                    timerOn = false;
+                    underSpell = false;
+                    myText.text = myRiddle.riddle;
+                }
+            }
+        }
+
         public void ChangeText(string value) {
+            StartInterfere(value);
+        }
+
+        private void StartInterfere(string value) {
             myText.text = value;
+            timer = 0;
+            timerOn = true;
+            underSpell = true;
+        }
+
+        public bool CanInterfear() {
+            return !underSpell;
+        }
+
+        public void Interfear() {
+            inputPanel.OpenPanel(this);
         }
     }
 }
