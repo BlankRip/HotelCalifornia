b38b59de557ec57d4040b8da80ea5e07307a69cd
Vishnu Raveendran <vishnu@khosouf.com>
2021-10-22 23:51:26 +0400

    fixing missing refs

--- Assets/Scripts/Network/RoomJoiner.cs
+++ Assets/Scripts/Network/RoomJoiner.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Knotgames.Network;
 using UnityEngine;
+using Knotgames.Extensions;
 
 namespace Knotgames.Network
 {
@@ -15,6 +16,31 @@ namespace Knotgames.Network
         public void Ready()
         {
             NetGameManager.instance.Ready();
+        }
+
+        public void LeaveRoom()
+        {
+            NetGameManager.instance.LeaveRoom();
+        }
+
+        public void CreateRoom()
+        {
+            NetGameManager.instance.CreateRoom();
+        }
+
+        public void Unready()
+        {
+            NetGameManager.instance.Unready();
+        }
+
+        public void JoinWithID()
+        {
+            NetGameManager.instance.JoinWithID();
+        }
+
+        public void CopyRoomID()
+        {
+            ClipboardExtensions.CopyToClipboard(NetRoomJoin.instance.roomID.value);
         }
     }
 }
