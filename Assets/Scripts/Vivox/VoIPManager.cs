1023d57379f74a3354cd5c8d3b6de79dbdfd6487
BlankRip <aniketh_g5@outlook.com>
2021-11-10 21:39:36 +0400

    Re adjusted player sizes and hope it is the last time

--- Assets/Scripts/Vivox/VoIPManager.cs
+++ Assets/Scripts/Vivox/VoIPManager.cs
@@ -1,89 +1,89 @@
-using UnityEngine;
-using VivoxUnity;
+// using UnityEngine;
+// using VivoxUnity;
 
-public class VoIPManager : MonoBehaviour
-{
-    static public VoIPManager instance;
-    static public string channelName = "default";
+// public class VoIPManager : MonoBehaviour
+// {
+//     static public VoIPManager instance;
+//     static public string channelName = "default";
 
-    [SerializeField]
-    private string server = "https://unity.vivox.com/appconfig/18966-hotel-63052-test";
-    [SerializeField]
-    private string domain = "mtu1xp.vivox.com";
-    [SerializeField]
-    private string tokenIssuer = "18966-hotel-63052-test";
-    [SerializeField]
-    private string tokenKey = "PcHv9TaUUmwRdVyzZMGriK1GKscMnsMF";
+//     [SerializeField]
+//     private string server = "https://unity.vivox.com/appconfig/18966-hotel-63052-test";
+//     [SerializeField]
+//     private string domain = "mtu1xp.vivox.com";
+//     [SerializeField]
+//     private string tokenIssuer = "18966-hotel-63052-test";
+//     [SerializeField]
+//     private string tokenKey = "PcHv9TaUUmwRdVyzZMGriK1GKscMnsMF";
 
-    [SerializeField]ChannelType channelType;
-
-
-    public bool audioDeviceMuted;
-
-    void Start()
-    {
-        if (instance == null) instance = this;
-        else
-        {
-            Destroy(gameObject);
-            return;
-        }
-
-        Init();
-        DontDestroyOnLoad(this.gameObject);
-    }
-
-    void OnDestroy()
-    {
-        VivoxVoiceManager.Instance.OnUserLoggedInEvent -= JoinCancel;
-    }
-
-    void Init()
-    {
-        Debug.Log("Initializing Vivox");
-        VivoxVoiceManager.Instance.SetCredentials(server, domain, tokenIssuer, tokenKey);
-        VivoxVoiceManager.Instance.OnUserLoggedInEvent += JoinCancel;
+//     [SerializeField]ChannelType channelType;
 
 
-        Invoke("CallLogin", 2);
-    }
+//     public bool audioDeviceMuted;
 
-    void CallLogin()
-    {
-        Login("displayName");
-    }
+//     void Start()
+//     {
+//         if (instance == null) instance = this;
+//         else
+//         {
+//             Destroy(gameObject);
+//             return;
+//         }
 
-    public void Mute()
-    {
-        audioDeviceMuted = !audioDeviceMuted;
-        VivoxVoiceManager.Instance.AudioInputDevices.Muted = audioDeviceMuted;
-    }
+//         Init();
+//         DontDestroyOnLoad(this.gameObject);
+//     }
 
-    public void JoinCancel()
-    {
-        VivoxVoiceManager.Instance.JoinChannel(channelName, channelType, VivoxVoiceManager.ChatCapability.AudioOnly, true, null);
-    }
+//     void OnDestroy()
+//     {
+//         VivoxVoiceManager.Instance.OnUserLoggedInEvent -= JoinCancel;
+//     }
 
-    void OnParticipantAdded(string userName, ChannelId channel, IParticipant participant)
-    {
-        Debug.Log("OnPartAdded: " + userName);
-        //UpdateParticipantRoster(participant, channel, true);
-    }
+//     void Init()
+//     {
+//         Debug.Log("Initializing Vivox");
+//         VivoxVoiceManager.Instance.SetCredentials(server, domain, tokenIssuer, tokenKey);
+//         VivoxVoiceManager.Instance.OnUserLoggedInEvent += JoinCancel;
 
-    void OnParticipantRemoved(string userName, ChannelId channel, IParticipant participant)
-    {
-        Debug.Log("OnPartRemoved: " + participant.Account.Name);
-        //UpdateParticipantRoster(participant, channel, false);
-    }
 
-    void Login(string displayName)
-    {
-        VivoxVoiceManager.Instance.Login(displayName);
-    }
+//         Invoke("CallLogin", 2);
+//     }
 
-    public void Logout()
-    {
-        VivoxVoiceManager.Instance.Logout();
-    }
+//     void CallLogin()
+//     {
+//         Login("displayName");
+//     }
 
-}
+//     public void Mute()
+//     {
+//         audioDeviceMuted = !audioDeviceMuted;
+//         VivoxVoiceManager.Instance.AudioInputDevices.Muted = audioDeviceMuted;
+//     }
+
+//     public void JoinCancel()
+//     {
+//         VivoxVoiceManager.Instance.JoinChannel(channelName, channelType, VivoxVoiceManager.ChatCapability.AudioOnly, true, null);
+//     }
+
+//     void OnParticipantAdded(string userName, ChannelId channel, IParticipant participant)
+//     {
+//         Debug.Log("OnPartAdded: " + userName);
+//         //UpdateParticipantRoster(participant, channel, true);
+//     }
+
+//     void OnParticipantRemoved(string userName, ChannelId channel, IParticipant participant)
+//     {
+//         Debug.Log("OnPartRemoved: " + participant.Account.Name);
+//         //UpdateParticipantRoster(participant, channel, false);
+//     }
+
+//     void Login(string displayName)
+//     {
+//         VivoxVoiceManager.Instance.Login(displayName);
+//     }
+
+//     public void Logout()
+//     {
+//         VivoxVoiceManager.Instance.Logout();
+//     }
+
+// }
