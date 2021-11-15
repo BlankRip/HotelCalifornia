ee66b57891dccac16fe7ff5ba052d21b95bfbe32
BlankRip <aniketh_g5@outlook.com>
2021-11-15 09:19:57 +0400

    Game Timer added

---
+++ Assets/Scripts/Gameplay/GameTimer.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace Knotgames.Gameplay {
+    public class GameTimer : MonoBehaviour
+    {
+        [SerializeField] GameplayEventCollection gameplayEvents;
+        [SerializeField] int minutes, seconds;
+        [SerializeField] TextMeshProUGUI minutesText, secondsText;
+        private int currenMinutes;
+        private float currentSeconds;
+        private bool timerOn;
+
+        private void Awake() {
+            currenMinutes = minutes;
+            minutesText.text = minutes.ToString();
+            currentSeconds = seconds;
+            secondsText.text = seconds.ToString();
+            gameplayEvents.gameStart.AddListener(StartTimer);
+        }
+
+        private void OnDestroy() {
+            gameplayEvents.gameStart.RemoveListener(StartTimer);
+        }
+
+        private void Update() {
+            if(Input.GetKeyDown(KeyCode.H)) {
+                gameplayEvents.gameStart.Invoke();
+            }
+            if(timerOn) {
+                if(currentSeconds > 0)
+                    currentSeconds -= Time.deltaTime;
+                else {
+                    if(currenMinutes > 0) {
+                        currenMinutes--;
+                        minutesText.text = currenMinutes.ToString();
+                        currentSeconds = 60;
+                        currentSeconds -= Time.deltaTime;
+                    } else {
+                        timerOn = false;
+                        Debug.Log("Gameover: Ghost wins");
+                    }
+                }
+                secondsText.text = ((int)currentSeconds).ToString();
+            }
+        }
+
+        private void StartTimer() {
+            timerOn = true;
+        }
+    }
+}
