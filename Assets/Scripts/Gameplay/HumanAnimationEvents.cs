3d96f9f407615cf677bf2defbd0fb733e8272b37
BlankRip <aniketh_g5@outlook.com>
2021-11-21 18:21:11 +0400

    Animations events and foot step sounds

--- Assets/Scripts/Gameplay/HumanAnimationEvents.cs
+++ Assets/Scripts/Gameplay/HumanAnimationEvents.cs
@@ -6,16 +6,23 @@ namespace Knotgames.Gameplay
 {
     public class HumanAnimationEvents : MonoBehaviour
     {
-        AudioSource footSource;
+        [SerializeField] AudioSource footSource;
         [SerializeField] AudioClip footstep;
+        private Animator unitySmallPP;
 
         void Start()
         {
             footSource = GetComponentInChildren<AudioSource>();
+            unitySmallPP = GetComponent<Animator>();
         }
 
-        void PlayFootstep()
-        {
+        void PlayFootstep() {
+            if(unitySmallPP.GetFloat("hSpeed") != 0 || unitySmallPP.GetFloat("vSpeed") != 0)
+                FootStepEffect();
+        }
+
+        void FootStepEffect() {
+            Debug.Log("FOOTED");
             footSource.PlayOneShot(footstep);
             footSource.pitch = Random.Range(1, 1.25f);
         }
