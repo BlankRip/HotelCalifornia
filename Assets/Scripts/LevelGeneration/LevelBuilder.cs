694f67b818b13162ea7aeef5067d5e0159bf4820
BlankRip <aniketh_g5@outlook.com>
2021-11-21 19:09:40 +0400

    Will this shit work

--- Assets/Scripts/LevelGeneration/LevelBuilder.cs
+++ Assets/Scripts/LevelGeneration/LevelBuilder.cs
@@ -74,16 +74,16 @@ namespace Knotgames.LevelGen {
             int x = seeder.levelSeed.GetSeed();
             Debug.LogError($"<color=red>SEED IS: {x}</color>");
 
-            PuzzleBuilder puzzleBuild = gameObject.AddComponent<PuzzleBuilder>();
-            puzzleBuild.Initilize(ref builderData);
-            IBuilder puzzleBuilder = puzzleBuild;
-            puzzleBuild.StartBuilder();
-            yield return interval;
-            while(puzzleBuild.GetBuilderStatus()) {
-                //?Debug.Log("<color=cyan>Puzzle Building</color>");
-                yield return interval;
-            }
-            Debug.Log("<color=yellow>Puzzles Built</color>");
+            // PuzzleBuilder puzzleBuild = gameObject.AddComponent<PuzzleBuilder>();
+            // puzzleBuild.Initilize(ref builderData);
+            // IBuilder puzzleBuilder = puzzleBuild;
+            // puzzleBuild.StartBuilder();
+            // yield return interval;
+            // while(puzzleBuild.GetBuilderStatus()) {
+            //     //?Debug.Log("<color=cyan>Puzzle Building</color>");
+            //     yield return interval;
+            // }
+            // Debug.Log("<color=yellow>Puzzles Built</color>");
             Debug.Log("<color=green>Level Built</color>");
             seeder.levelSeed.SeedSuccesful();
 
