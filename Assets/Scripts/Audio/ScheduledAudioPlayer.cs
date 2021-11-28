bd0678094530e4ba0e8a448eaf650d823d3e24b1
Vishnu Raveendran <vishnu@khosouf.com>
2021-11-28 12:14:27 +0400

    morse puzzle fixed

--- Assets/Scripts/Audio/ScheduledAudioPlayer.cs
+++ Assets/Scripts/Audio/ScheduledAudioPlayer.cs
@@ -6,37 +6,18 @@ namespace Knotgames.Audio
 {
     public class ScheduledAudioPlayer : MonoBehaviour
     {
-        private double startTime;
-        private int toggle = 0;
-        private int nextClip = 0;
-        public AudioSource[] musicSources;
-        public List<AudioClip> songsToPlay;
+        public List<AudioClip> songsToPlay = new List<AudioClip>();
+        public AudioSource audioSource;
 
-        private void Start()
+        private void Update()
         {
-            startTime = AudioSettings.dspTime;
-        }
-
-        void Update()
-        {
-            if (AudioSettings.dspTime > startTime - 1)
+            if (audioSource.isPlaying == false)
             {
-                Debug.Log("playing");
-                //Check if there is an empty audio Clip
-                if (songsToPlay[nextClip] != null)
-                {
-                    AudioClip clipToPlay = songsToPlay[nextClip];
-                    // Loads the next Clip to play and schedules when it will start
-                    musicSources[toggle].clip = clipToPlay;
-                    musicSources[toggle].PlayScheduled(startTime);
-                    // Checks how long the Clip will last and updates the Next Start Time with a new value
-                    double duration = (double)clipToPlay.samples / clipToPlay.frequency;
-                    startTime = startTime + duration;
-                    // Switches the toggle to use the other Audio Source next
-                    toggle = 1 - toggle;
-                }
-                // Increase the clip index number, reset if it runs out of clips
-                nextClip = nextClip < songsToPlay.Count - 1 ? nextClip + 1 : 0;
+                AudioClip clip = songsToPlay[0];
+                audioSource.clip = clip;
+                songsToPlay.RemoveAt(0);
+                songsToPlay.Add(clip);
+                audioSource.Play();
             }
         }
     }
