8f1041596835286676aa7b7b9d16ca97c832c273
Vishnu Raveendran <vishnu@khosouf.com>
2021-12-09 01:16:39 +0400

    fixed level shit

--- Assets/Scripts/Settings/MouseSettings.cs
+++ Assets/Scripts/Settings/MouseSettings.cs
@@ -46,7 +46,7 @@ namespace Knotgames.GameSettings
                 senseFloat.value = x;
                 currentSense = x;
                 senseSlider.value = x;
-                senseText.text = x.ToString();
+                senseText.text = x.ToString("F2");
                 SaveSettings();
             }
         }
